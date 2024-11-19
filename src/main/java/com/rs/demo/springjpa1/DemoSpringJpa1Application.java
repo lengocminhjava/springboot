1f55537c81444ab8fe5448a016f90d6780a0eca4
lengocminhjava <minhokmen2@gmail.com>
2024-11-19 22:21:50 +0700

    first java spring

---
+++ src/main/java/com/rs/demo/springjpa1/DemoSpringJpa1Application.java
@@ -0,0 +1,13 @@
+package com.rs.demo.springjpa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringJpa1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringJpa1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/rs/demo/springjpa1/DemoSpringJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs.demo.springjpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringJpa1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
